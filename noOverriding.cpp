f3f2f00048397573b921698d6c27f3d1d5a49de1
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-03 18:49:30 +0700

    Membuat bderivedClass

--- noOverriding.cpp
+++ noOverriding.cpp
@@ -7,3 +7,18 @@ public:
         cout << "Hallo saya function dari base class";
     }
 };
+
+class derivedClass : public baseClass {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class":
+
+    }
+};
+
+int main() {
+        derivedClass a;
+        a.perkenalan();
+
+        return 0;
+}
