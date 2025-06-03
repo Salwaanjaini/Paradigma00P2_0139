7f1efaa4d4e1ed4860cf940a16d7bf0a602b15e5
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-03 18:18:14 +0700

    Membuat class lampu rumah

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -9,4 +9,17 @@ public:
         saklarNo[i] = value;
         
     }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0,"Lampu Teras Rumah");
+    lampuRumah.setSaklarNo (1,"Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2,"Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+    
 }
